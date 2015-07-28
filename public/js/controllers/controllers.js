529d9bcc556a6c43c2ecce2180f36a8db47261a0
Guilherme Solinscki <guilherme.slnk@gmail.com>
2015-07-28 00:28:06 -0300

    Add task function added

--- public/js/controllers/controllers.js
+++ public/js/controllers/controllers.js
@@ -2,4 +2,16 @@ var todoControllers = angular.module('todoControllers', []);
 
 todoControllers.controller('TaskList', ['$scope', 'Task', function($scope, Task) {
     $scope.tasks = Task.query();
+    $scope.description = '';
+
+    $scope.addTask = function() {
+        var task = new Task({
+            description: $scope.description
+        });
+
+        task.$save();
+
+        $scope.tasks = Task.query();
+        $scope.description = '';
+    }
 }]);
