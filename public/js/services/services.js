6f1ce8ce9ec739bc951d053c5ae7c654d56d2fce
Guilherme Solinscki <guilherme.slnk@gmail.com>
2015-08-15 19:05:39 -0300

    Resource patch method added

--- public/js/services/services.js
+++ public/js/services/services.js
@@ -1,5 +1,8 @@
 var todoServices = angular.module('todoServices', ['ngResource']);
 
 todoServices.factory('Task', ['$resource', function($resource) {
-    return $resource('/tasks');
+    return $resource('/tasks/:id', null, 
+        {
+            'update': { method: 'PATCH' }
+        });
 }]);
