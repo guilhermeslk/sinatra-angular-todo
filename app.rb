64a17b7bf1a5670a3e962027a712ccf379b6ef2d
Guilherme Solinscki <guilherme.slnk@gmail.com>
2016-01-15 13:34:55 -0200

    Fix indentation

--- app.rb
+++ app.rb
@@ -4,52 +4,52 @@ require 'active_record'
 require 'json'
 
 ActiveRecord::Base.establish_connection(
-    :adapter => 'sqlite3',
-    :database => 'todo.sqlite3.db'
+  :adapter => 'sqlite3',
+  :database => 'todo.sqlite3.db'
 )
 
 class Task < ActiveRecord::Base
 end
 
 get '/' do
-    erb :index
+  erb :index
 end
 
 get '/tasks' do
-    Task.all().to_json
+  Task.all().to_json
 end
 
 get '/tasks/:id' do
-    Task.find(params[:id]).to_json
+  Task.find(params[:id]).to_json
 end
 
 post '/tasks' do
-    @data = JSON.parse(request.body.read)
+  @data = JSON.parse(request.body.read)
 
-    if @data['description'].nil?
-        halt 400, 'Malformed JSON'
-    end
+  if @data['description'].nil?
+    halt 400, 'Malformed JSON'
+  end
 
-    @task = Task.create(
-        'description' => @data['description'],
-        'done' => false 
-    )
+  @task = Task.create(
+    'description' => @data['description'],
+    'done' => false
+  )
 
-    @task.to_json
+  @task.to_json
 end
 
 patch '/tasks/:id' do
-    @task = Task.find(params[:id])
+  @task = Task.find(params[:id])
 
-    if @task.nil?
-        halt 404, 'Task not found'
-    end
+  if @task.nil?
+    halt 404, 'Task not found'
+  end
 
-    @data = JSON.parse(request.body.read)
+  @data = JSON.parse(request.body.read)
 
-    @task.description = @data['description']
-    @task.done = @data['done']
+  @task.description = @data['description']
+  @task.done = @data['done']
 
-    @task.save
-    @task.to_json
+  @task.save
+  @task.to_json
 end
